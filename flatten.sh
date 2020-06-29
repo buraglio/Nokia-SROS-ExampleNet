c0711b76535bb8e76ac060334b5a0dc479f00efb
Nick Buraglio <nick@buraglio.com>
2020-06-29 16:31:13 -0500

    Initial commit

---
+++ flatten.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Requires transcode-sros
+# adust paths accordingly
+
+/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr1 > vsr1-flat.txt
+/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr2 > vsr2-flat.txt
+/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr3 > vsr3-flat.txt
+/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr4 > vsr4-flat.txt
+#/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr5 > vsr5-flat.txt
+#/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr6 > vsr6-flat.txt
+#/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr-nrc1 > vsr-nrc1-flat.txt
+#/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr-nrc2 > vsr-nrc2-flat.txt
+#/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsr-nrc1-example > vsr-nrc1-example-flat.txt
+#/usr/bin/python ~/SynologyDrive/Dev/transcode-sros/trancode-sros.py vsri8 > vsri8-flat.txt
